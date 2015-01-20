0f5dfec6a90341016855b175e21574b6869fd2f4
DoumanAsh <custparasite@gmx.se>
2015-01-20 08:44:50 +0300

    Correction to code_trim

--- python/code_trim.py
+++ python/code_trim.py
@@ -1,38 +1,33 @@
+""" Script to remove tailing spaces in provided dir/file """
+
 #!/usr/bin/python
-#remove tailing spaces in provided dir/file
 
 import os
 from sys import argv
 from shutil import copy2
 
 def trimm_file(filename):
+    """ Trimms file of white spaces """
     backup = filename + ".old"
     copy2(filename, backup)
-    try:
-        with open(backup, 'r') as input_file, open(filename, 'w') as output_file:
-            for line in input_file:
-                #\n is interpreted according to platform in text mode
-                output_file.write(line.rstrip() + '\n')
-        os.remove(backup)
-    except Exception as e:
-        print(">>>Exception: " + str(e))
-        print("Something wrong with " + filename)
-        print("Rollback changes")
-        if os.path.isfile(filename):
-            os.remove(filename)
-        os.rename(backup, filename)
+    with open(backup, 'r') as input_file, open(filename, 'w') as output_file:
+        for line in input_file:
+            #\n is interpreted according to platform in text mode
+            output_file.write(line.rstrip() + '\n')
+    os.remove(backup)
 
 def trimmer():
+    """ Run through arguments to apply trimm_file() """
     args = argv[1:]
     for arg in args:
         if os.path.isfile(arg):
             trimm_file(arg)
         elif os.path.isdir(arg):
-            for path, dirs, files in os.walk(arg):
-                for f in files:
-                    file_name, file_extension = os.path.splitext(f)
+            for path, _, files in os.walk(arg):
+                for proc_file in files:
+                    _, file_extension = os.path.splitext(proc_file)
                     if not file_extension == '':
-                        trimm_file(os.path.join(path, f))
+                        trimm_file(os.path.join(path, proc_file))
         else:
             print("".join((arg, " is not found")))
 
