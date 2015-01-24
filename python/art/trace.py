9d550c4a63a5241a36f6a5c4f4acb42bcd844928
DoumanAsh <custparasite@gmx.se>
2015-01-24 11:21:37 +0300

    [trace] Re-factor timestamp handling

--- python/art/trace.py
+++ python/art/trace.py
@@ -25,6 +25,11 @@ class EventTracer():
         event = event.upper()
         trace_to = ""
 
+        #timestamp
+        try:
+            timestamp = str(self.time()) + " "
+        except TypeError:
+            timestamp = ""
         #function name and line of caller
         frame = currentframe().f_back
         func_line = ":".join(("".join((frame.f_code.co_name, "()")), str(frame.f_lineno)))
@@ -32,18 +37,14 @@ class EventTracer():
         try:
             if self.events[event] and argv:
                 #header: date and time + {EVENT}
-                trace_to = "".join(("{", event, "}"))
-                if self.time:
-                    trace_to = self.sep.join((str(self.time()), trace_to))
+                trace_to = "".join((timestamp, "{", event, "}"))
                 #compose trace
                 trace_to = self.sep.join((trace_to, "-", func_line, "-",
                                           " ".join(str(arg) for arg in argv)))
 
         except KeyError:
-            trace_to = self.sep.join(("{ERROR}", "-", func_line, "-",
+            trace_to = self.sep.join(("".join((timestamp, "{ERROR}")), "-", func_line, "-",
                                       "Unexpected event:", event, trace_to, "| Args:", str(argv)))
-            if self.time:
-                self.sep.join((str(self.time()), trace_to))
 
         if trace_to:
             print(trace_to)
