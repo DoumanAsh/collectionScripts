a050ced55cb102d55989a8faf85431968ec8a359
DoumanAsh <custparasite@gmx.se>
2015-01-30 01:59:30 +0300

    [trace] add extended EventTracer

--- python/art/trace.py
+++ python/art/trace.py
@@ -74,3 +74,89 @@ class EventTracer():
             self.time = datetime.now
         else:
             self.time = None
+
+
+class EventTracer3():
+    """ Event Tracer
+
+        Provides shortcuts for trace events:
+        self.[event]()
+    """
+    def __init__(self, delimiter=" ", time=False):
+        self.sep = delimiter
+        self.time = None
+        if time:
+            self.time = datetime.now
+        self.events = {"ERROR"   : True,
+                       "WARNING" : True,
+                       "INFO"    : False,
+                       "DEBUG"   : False}
+        for name, value in self.events.items():
+            self.set_event(name, value)
+
+    def reset(self):
+        """ Reset events to initial state. """
+        self.__init__()
+
+    def trace(self, event, *argv, frame=None):
+        """ Prints trace.
+
+            Format: [timestamp] {event} - filename:line - func(): trace
+        """
+        event = event.upper()
+        trace_to = ""
+
+        try:
+            if self.events[event] and argv:
+                #timestamp
+                timestamp = ""
+                if self.time:
+                    timestamp = " ".join((str(self.time()), timestamp))
+                event = "".join((timestamp, "{", event, "}"))
+                #filename + line
+                if not frame:
+                    frame = currentframe()
+                frame = frame.f_back
+                file_name = frame.f_code.co_filename
+                file_line = ":".join((file_name, str(frame.f_lineno)))
+                func_name = "".join((frame.f_code.co_name, "():"))
+
+                trace_to = self.sep.join((event, "-", file_line, "-", func_name,
+                                          " ".join(str(arg) for arg in argv)))
+
+        except KeyError:
+            #timestamp
+            timestamp = ""
+            if self.time:
+                timestamp = " ".join((str(self.time()), timestamp))
+            #filename + line
+            frame = frame.f_back
+            file_name = frame.f_code.co_filename
+            file_line = ":".join((file_name, str(frame.f_lineno)))
+            func_name = "".join((frame.f_code.co_name, "():"))
+
+            trace_to = self.sep.join(("".join((timestamp, "{ERROR}")),
+                                      "-", file_line, "-", func_name,
+                                      "Unexpected event:", event, trace_to, "| Args:", str(argv)))
+
+        if trace_to:
+            print(trace_to)
+
+    def set_event(self, name, enabled=True):
+        """ Add event to table if there is no such event.
+            Otherwise updates event's state.
+        """
+        name = name.upper()
+        self.events[name] = enabled
+        #add shortcut for new event
+        if not hasattr(self, name):
+            def __trace(self, *argv):
+                self.trace(name, *argv, frame=currentframe())
+            setattr(self.__class__, name, __trace)
+
+    def set_time(self, enable=True):
+        """ Enable or disable timestamp. """
+        if enable:
+            self.time = datetime.now
+        else:
+            self.time = None
