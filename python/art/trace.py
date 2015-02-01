bc65b1c7a278bd5ce1193f38cb18bfe43536a2a8
DoumanAsh <custparasite@gmx.se>
2015-02-01 23:25:47 +0300

    [trace] Final version?

--- python/art/trace.py
+++ python/art/trace.py
@@ -4,85 +4,12 @@ from datetime import datetime
 from inspect import currentframe
 
 class EventTracer():
-    """ Event Tracer """
-    def __init__(self, delimiter=" ", time=False):
-        self.sep = delimiter
-        self.time = None
-        if time:
-            self.time = datetime.now
-        self.events = {"ERROR"   : True,
-                       "WARNING" : True,
-                       "INFO"    : False,
-                       "DEBUG"   : False}
-
-    def reset(self):
-        """ Reset events to initial state. """
-        self.__init__()
-
-    def trace(self, event, *argv):
-        """ Prints trace.
-
-            Format: [timestamp] {event} - filename:line - func(): trace
-        """
-        event = event.upper()
-        trace_to = ""
-
-        try:
-            if self.events[event] and argv:
-                #timestamp
-                timestamp = ""
-                if self.time:
-                    timestamp = " ".join((str(self.time()), timestamp))
-                event = "".join((timestamp, "{", event, "}"))
-                #filename + line
-                frame = currentframe().f_back
-                file_name = frame.f_code.co_filename
-                file_line = ":".join((file_name, str(frame.f_lineno)))
-                func_name = "".join((frame.f_code.co_name, "()"))
-
-                trace_to = self.sep.join((event, "-", " ".join(str(arg) for arg in argv),
-                                          "[", func_name, "-", file_line, "]"))
-
-        except KeyError:
-            #timestamp
-            timestamp = ""
-            if self.time:
-                timestamp = " ".join((str(self.time()), timestamp))
-            #filename + line
-            frame = currentframe().f_back
-            file_name = frame.f_code.co_filename
-            file_line = ":".join((file_name, str(frame.f_lineno)))
-            func_name = "".join((frame.f_code.co_name, "()"))
-
-            trace_to = self.sep.join(("".join((timestamp, "{ERROR}")), "-",
-                                      "Unexpected event:", event, trace_to, "| Args:", str(argv),
-                                      "[", func_name, "-", file_line, "]"))
-
-        if trace_to:
-            print(trace_to)
-
-    def set_event(self, name, enabled=True):
-        """ Add event to table if there is no such event.
-            Otherwise updates event's state.
-        """
-        name = name.upper()
-        self.events[name] = enabled
-
-    def set_time(self, enable=True):
-        """ Enable or disable timestamp. """
-        if enable:
-            self.time = datetime.now
-        else:
-            self.time = None
-
-
-class EventTracer3():
     """ Event Tracer
 
         Provides shortcuts for trace events:
         self.[event]()
     """
-    def __init__(self, delimiter=" ", time=False):
+    def __init__(self, delimiter=" ", time=False, file_name=None):
         self.sep = delimiter
         self.time = None
         if time:
@@ -91,10 +18,32 @@ class EventTracer3():
         self.events = dict()
         for name, value in (("ERROR", True), ("WARNING", True), ("INFO", False), ("DEBUG", False)):
             self.set_event(name, value)
+        self.log_fd = None
+        if file_name:
+            try:
+                self.log_fd = open(file_name, 'a')
+                self.__init_log_fd__()
+            except IOError as errno:
+                self.trace("ERROR", "Not possible to use log file:", file_name, "| Error:", errno)
 
     def reset(self):
         """ Reset events to initial state. """
         self.__init__()
+
+    def __init_log_fd__(self):
+        """ Function to write log header """
+        self.log_fd.write("================= Event log =================\n")
+        self.log_fd.write("=== Start time:{} ===\n".format(datetime.now()))
+        self.log_fd.write("=============================================\n")
+
+    def close(self):
+        """ Closes logging class
+            Should be called before application closure
+        """
+        if self.log_fd:
+            self.log_fd.write("=============================================\n")
+            self.log_fd.write("=== End time: {} ===\n".format(datetime.now()))
+            self.log_fd.close()
 
     def trace(self, event, *argv):
         """ Prints trace.
@@ -105,7 +54,7 @@ class EventTracer3():
         trace_to = ""
 
         try:
-            if self.events[event] and argv:
+            if self.events[event]:
                 #timestamp
                 timestamp = ""
                 if self.time:
@@ -141,6 +90,8 @@ class EventTracer3():
 
         if trace_to:
             print(trace_to)
+            if self.log_fd:
+                self.log_fd.write("".join((trace_to, '\n')))
 
     def set_event(self, name, enabled=True):
         """ Add event to table if there is no such event.
@@ -163,3 +114,16 @@ class EventTracer3():
             self.time = datetime.now
         else:
             self.time = None
+
+    def set_log_file(self, file_name):
+        """ Initiate logging to file.
+
+            @param file_name Valid and absolute name of file.
+        """
+        if self.log_fd:
+            self.close()
+        try:
+            self.log_fd = open(file_name, 'a')
+            self.__init_log_fd__()
+        except IOError as errno:
+            self.trace("ERROR", "Not possible to use log file:", file_name, "| Error:", errno)
