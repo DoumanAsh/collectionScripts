caec7946e335942af564e813fa60cfe9d3de0b8b
DoumanAsh <custparasite@gmx.se>
2015-04-29 20:05:15 +0300

    Update git macro for lgit script

--- rust/lgit.rs
+++ rust/lgit.rs
@@ -3,10 +3,9 @@ use std::env::args as cmd_args;
 use std::process::Command;
 
 ///Executes git command with arguments
+///Building sequence of arguments like .arg(@arg)
 macro_rules! exec_git_cmd {
-    ($args:expr) => {
-        Command::new("git").args($args).status().unwrap();
-    }
+    ($($arg:expr),*) => { Command::new("git")$(.arg($arg))*.status().unwrap(); }
 }
 
 #[inline(always)]
@@ -23,32 +22,32 @@ fn usage() {
 ///Amend changes
 fn git_amend(args: &[String]) {
     git_add();
-    if args.len() != 0 && args[0] == "edit" { exec_git_cmd!(&["commit", "--amend"]); }
-    else { exec_git_cmd!(&["commit", "--amend", "--no-edit"]); }
+    if args.len() != 0 && args[0] == "edit" { exec_git_cmd!("commit", "--amend"); }
+    else { exec_git_cmd!("commit", "--amend", "--no-edit"); }
 }
 
 ///Fetch changes from upstream
 fn git_fetch() {
-    exec_git_cmd!(&["checkout", "master"]);
-    exec_git_cmd!(&["fetch", "upstream"]);
-    exec_git_cmd!(&["merge", "upstream/master"]);
+    exec_git_cmd!("checkout", "master");
+    exec_git_cmd!("fetch", "upstream");
+    exec_git_cmd!("merge", "upstream/master");
 }
 
 ///Push the current branch
 fn git_push(args: &[String]) {
-    if args.len() != 0 && args[0] =="force" { exec_git_cmd!(&["push", "--force", "origin", "HEAD"]); }
-    else { exec_git_cmd!(&["push", "origin", "HEAD"]); }
+    if args.len() != 0 && args[0] =="force" { exec_git_cmd!("push", "--force", "origin", "HEAD"); }
+    else { exec_git_cmd!("push", "origin", "HEAD"); }
 }
 
 ///Add all changes
 fn git_add() {
-    exec_git_cmd!(&["add", "--all"]);
+    exec_git_cmd!("add", "--all");
 }
 
 ///Undo all changes
 fn git_clean() {
-    exec_git_cmd!(&["checkout", "-f"]);
-    exec_git_cmd!(&["clean", "-fdq"]);
+    exec_git_cmd!("checkout", "-f");
+    exec_git_cmd!("clean", "-fdq");
 }
 
 ///Unexpected argument handler
@@ -60,24 +59,23 @@ fn unexpected(arg: &str) {
 fn main() {
     let args: Vec<String> = cmd_args().collect();
     //exclude file name
-    let s_args = &args[1..];
     let is_repo = Command::new("git").arg("rev-parse").arg("-q").output().unwrap();
 
     if is_repo.status.code().unwrap() != 0 {
         println!("Not a git repository");
         return;
     }
-    else if s_args.len() == 0 {
+    else if args.len() == 1 {
         usage();
         return;
     }
 
-    match s_args[0].as_ref() {
+    match args[1].as_ref() {
         "amend" => git_amend(&args[2..]),
         "fetch" => git_fetch(),
         "push"  => git_push(&args[2..]),
         "add"   => git_add(),
         "clean" => git_clean(),
-        _       => unexpected(&s_args[0]),
+        _       => unexpected(&args[1]),
     }
 }
