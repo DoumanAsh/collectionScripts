089a7b69305285cb857b0c573b8d75f6c591fe75
DoumanAsh <custparasite@gmx.se>
2015-05-02 17:17:02 +0300

    Add tiny_png python script

---
+++ python/tiny_png.py
@@ -0,0 +1,59 @@
+""" PNG/JPG compression via tinypng.com
+
+    Get API key https://tinypng.com/developers
+"""
+
+#!/usr/bin/env python3
+
+from sys import argv
+from os import mkdir
+from os.path import dirname
+from os.path import basename
+from os.path import isfile
+from os.path import isdir
+from os.path import join as path_join
+from base64 import b64encode
+from http.client import HTTPSConnection as https
+
+def main(args):
+    if not args:
+        return
+
+    key = "" #enter API key
+    auth = b64encode(bytes("api:" + key, "ascii")).decode("ascii")
+    headers = {"Authorization" : " ".join(("Basic", auth))}
+    connection = https("api.tinypng.com")
+    for arg in args:
+        print(" ".join((">>>Shrink pic:", arg)))
+        if not isfile(arg):
+            print("".join((">>>File: ", arg, " does not exist")))
+
+        orig_file, ext = arg.rsplit(".", 1)
+        if not (ext == "png" or ext == "jpeg" or ext == "jpg"):
+            print("".join((">>>File ", arg, " is not png or jpg/jpeg")))
+
+
+        result_dir = path_join(dirname(orig_file), "tiny_png_optimized")
+        if not isdir(result_dir):
+            mkdir(result_dir)
+        output = path_join(result_dir, basename(arg))
+
+        connection.request("POST", "https://api.tinypng.com/shrink", open(arg, "rb").read(), headers)
+        response = connection.getresponse()
+        if response.status == 201:
+            # Compression was successful, retrieve output from Location header.
+            response.read()
+            connection.request("GET", response.getheader("Location"))
+            result = connection.getresponse()
+            open(output, "wb").write(result.read())
+            print(" ".join(("Succesfuly shrinked. Result pic:", output)))
+        else:
+            # Something went wrong! You can parse the JSON body for details.
+            print(" ".join(("Failed to compress:", arg, "Status:", str(response.status))))
+            print(" ".join(("Reason:", response.reason)))
+            response.read()
+
+
+if __name__ == "__main__":
+    main(argv[1:])
+    input("Press any key...")
