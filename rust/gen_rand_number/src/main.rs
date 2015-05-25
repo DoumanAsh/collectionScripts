7e0061cc754c3d71595b0f6540e5358b373f2d82
DoumanAsh <custparasite@gmx.se>
2015-05-26 00:56:08 +0300

    Rust: gen_rand_number
    Generates specified number of digists

---
+++ rust/gen_rand_number/src/main.rs
@@ -0,0 +1,32 @@
+extern crate rand;
+use rand::thread_rng;
+use rand::distributions::{IndependentSample, Range};
+use std::env::args as cmd_args;
+
+#[inline(always)]
+fn usage() {
+    println!("Usage:");
+    println!("    gen_random [number of digits]");
+}
+
+fn gen_random_number_string(number: usize) -> String {
+    let num_range = Range::new(0, 9);
+    let mut rng = rand::thread_rng();
+    let mut final_string = String::with_capacity(number);
+    for _ in 0..number {
+        final_string.push_str(format!("{}", num_range.ind_sample(&mut rng)).as_ref());
+    }
+    final_string
+}
+
+fn main() {
+    if let Some(arg1) = cmd_args().skip(1).next() {
+        if let Ok(arg1_number) = arg1.parse::<usize>() {
+            println!("Got number: {}", gen_random_number_string(arg1_number));
+        }
+        else {
+            println!("Cannon parse >{}< into positive number", &arg1);
+        }
+    }
+    else { usage() }
+}
