eb28614c39bd2263c0e52a99d321d18a269da81a
DoumanAsh <custparasite@gmx.se>
2015-06-26 23:10:54 +0300

    powershell: update functions

--- powershell/ps_function.ps1
+++ powershell/ps_function.ps1
@@ -2,3 +2,6 @@ function prompt { "[$($executionContext.SessionState.Path.CurrentLocation)]$("`n
 function which($name){Get-Command $name | Select-Object -ExpandProperty Definition}
 function readlink($name) {Get-Item $name | select -ExpandProperty FullName }
 function pwd {Get-Location | select -ExpandProperty Path}
+function gitk {wish $(Join-Path $(Split-Path $(which git)) gitk) }
+function cxfreeze {python E:\Soft\Misc\chocolatey\bin\python\Scripts\cxfreeze $args}
+function ctags_rust {ctags --options=E:\Soft\Develop\Rust\etc\ctags.rust $args}
