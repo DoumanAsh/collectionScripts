809f7f57bc325632221cddb59c215ef43ee72e8e
DoumanAsh <custparasite@gmx.se>
2015-06-29 21:46:21 +0300

    powershell: add function to get clipboard text

--- powershell/ps_function.ps1
+++ powershell/ps_function.ps1
@@ -5,3 +5,12 @@ function pwd {Get-Location | select -ExpandProperty Path}
 function gitk {wish $(Join-Path $(Split-Path $(which git)) gitk) }
 function cxfreeze {python E:\Soft\Misc\chocolatey\bin\python\Scripts\cxfreeze $args}
 function ctags_rust {ctags --options=E:\Soft\Develop\Rust\etc\ctags.rust $args}
+
+function Get-ClipboardText()
+{
+	Add-Type -AssemblyName System.Windows.Forms
+	$tb = New-Object System.Windows.Forms.TextBox
+	$tb.Multiline = $true
+	$tb.Paste()
+	$tb.Text
+}
