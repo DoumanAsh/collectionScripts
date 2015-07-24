eb2867e9c8eb0d58030c6e7d9459be5a9b6ef49b
Douman <custparasite@gmx.se>
2015-07-24 17:08:49 +0300

    Clipboard_monitor: trim right only

--- rust/clipboard_monitor/src/main.rs
+++ rust/clipboard_monitor/src/main.rs
@@ -6,39 +6,39 @@ use std::process::Command;
 ///Trimm all lines in string.
 ///Return None if string is not changed.
 fn trim_lines(text: &String) -> Option<String> {
-let orig_len = text.len();
-let last_char = text.chars().last().unwrap();
-let mut text = text.lines().fold(String::new(), |acc, line| acc + line.trim() + "\n");
+    let orig_len = text.len();
+    let last_char = text.chars().last().unwrap();
+    let mut text = text.lines().fold(String::new(), |acc, line| acc + line.trim_right() + "\n");
 
-if last_char != '\n' {
-text.pop();
-}
+    if last_char != '\n' {
+        text.pop();
+    }
 
-if orig_len == text.len() { return None; }
-Some(text)
+    if orig_len == text.len() { return None; }
+    Some(text)
 }
 
 fn handler_clip_text(text: &String) {
-if text.len() == 0 { return; }
+    if text.len() == 0 { return; }
 
-if text.starts_with("wget") {
-println!("wget download:");
-let status = Command::new("powershell").arg("-Command").arg(format!("cd D:/Downloads; {}", &text)).status().unwrap();
-if !status.success() {
-println!("Failed to run wget.");
-}
-return;
-}
+    if text.starts_with("wget") {
+        println!("wget download:");
+        let status = Command::new("powershell").arg("-Command").arg(format!("cd D:/Downloads; {}", &text)).status().unwrap();
+        if !status.success() {
+            println!("Failed to run wget.");
+        }
+        return;
+    }
 
-println!("Clipboard content:\n{}", &text);
-if let Some(trim_text) = trim_lines(&text) {
-if set_clipboard(&trim_text).is_err() {
-println!("Failed to update clipboard");
-}
-}
+    println!("Clipboard content:\n{}", &text);
+    if let Some(trim_text) = trim_lines(&text) {
+        if set_clipboard(&trim_text).is_err() {
+            println!("Failed to update clipboard");
+        }
+    }
 }
 
 fn main() {
-println!("Clipboard monitor");
-ClipboardManager::new().ok_callback(handler_clip_text).run();
+    println!("Clipboard monitor");
+    ClipboardManager::new().ok_callback(handler_clip_text).run();
 }
