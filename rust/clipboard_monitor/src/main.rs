c6a78b10a7a0628f6f5d5cac8c296670081b0802
DoumanAsh <custparasite@gmx.se>
2015-07-24 20:32:12 +0300

    clipboard_monitor: parallel download

--- rust/clipboard_monitor/src/main.rs
+++ rust/clipboard_monitor/src/main.rs
@@ -3,6 +3,16 @@ extern crate clipboard_win;
 use clipboard_win::{set_clipboard, ClipboardManager};
 use std::process::Command;
 
+#[inline(always)]
+fn find_file_name(text: &String) -> String {
+    let left: usize = text.rfind("-O \"").unwrap_or(0);
+    let right: usize = text.rfind("\" -c").unwrap_or(0);
+
+    if left == right { return "UNKNOWN".to_string(); }
+    let left: usize = left + 4;
+    text[left..right].to_string()
+}
+
 ///Trimm all lines in string.
 ///Return None if string is not changed.
 fn trim_lines(text: &String) -> Option<String> {
@@ -22,10 +32,20 @@ fn handler_clip_text(text: &String) {
     if text.len() == 0 { return; }
 
     if text.starts_with("wget") {
-        println!("wget download:");
-        let status = Command::new("powershell").arg("-Command").arg(format!("cd D:/Downloads; {}", &text)).status().unwrap();
-        if !status.success() {
-            println!("Failed to run wget.");
+        let file_name = find_file_name(&text);
+        println!(">>>wget {}", &file_name);
+
+        let cmd = format!("cd E:/Downloads; {}", text);
+        if std::thread::Builder::new().name(file_name).spawn(move || {
+            let output = Command::new("powershell").arg("-Command").arg(cmd).output().unwrap();
+            if !output.status.success() {
+                println!("[{}] Failed to wget:\n{}", std::thread::current().name().unwrap_or("UNKNOWN"), String::from_utf8_lossy(&output.stdout));
+            }
+            else {
+                println!("[{}] Successfully downloaded", std::thread::current().name().unwrap_or("UNKNOWN"));
+            }
+        }).is_err() {
+            println!("Failed to run new thread with wget");
         }
         return;
     }
