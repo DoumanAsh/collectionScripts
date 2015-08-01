24032a3a1067f09cd319cdeed520937aa924c1eb
Douman <douman@gmx.se>
2015-08-02 01:23:21 +0300

    powershell: set_alias function

--- powershell/ps_function.ps1
+++ powershell/ps_function.ps1
@@ -5,6 +5,22 @@ function pwd {Get-Location | select -ExpandProperty Path}
 function gitk {wish $(Join-Path $(Split-Path $(which git)) gitk) }
 function cxfreeze {python E:\Soft\Misc\chocolatey\bin\python\Scripts\cxfreeze $args}
 function ctags_rust {ctags --options=E:\Soft\Develop\Rust\etc\ctags.rust $args}
+#usage: ". set_alias <name, value[, scope]>..
+#separator is whie-space
+function set_alias() {
+    foreach ($arg in $args) {
+        $params = $arg.split(",")
+        $name = $arg[0]
+        $value = $arg[1]
+        if ($parmas.Length -lt 3) {
+            $scope = "AllScope"
+        }
+        else {
+            $scope = $arg[2]
+        }
+        New-Alias -Name "$name" -Value $value -Option "$scope" -Force
+    }
+}
 
 function Get-ClipboardText()
 {
