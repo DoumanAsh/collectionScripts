f66106347688162da7b71570af2ec3b770d78217
Douman <douman@gmx.se>
2015-09-05 20:22:26 +0300

    Rust: senkou improve text extraction

--- rust/clipboard_monitor/src/senkou.rs
+++ rust/clipboard_monitor/src/senkou.rs
@@ -7,24 +7,36 @@ use clipboard_win::{set_clipboard, ClipboardManager};
 
 fn handler_clip_text(text: &String) {
     let len = text.len();
-    let len_half = len / 2;
     if len == 0 || len % 2 == 1 { return; }
 
-    let chars: Vec<char> = text.chars().collect();
-    if !chars.starts_with(&chars[chars.len()/2..]) { return; }
+    const BEGIN: &'static[char] = &['「', '（'];
+    const END: &'static[char] = &['」', '）'];
+    if let (Some(begin_pos), Some(end_pos)) = (text.rfind(BEGIN), text.rfind(END)) {
+        println!(">>>Action:");
+        //+3 to skip begin symbol
+        if set_clipboard(&text[begin_pos+3..end_pos]).is_err() {
+            println!("Failed to update clipboard");
+        }
 
-    drop(chars);
-    println!(">>>Action:");
-    if set_clipboard(&text[..len_half]).is_err() {
-        println!("Failed to update clipboard");
+        println!("Dialogue is extracted");
     }
+    else {
+        let chars: Vec<char> = text.chars().collect();
+        if !chars.starts_with(&chars[chars.len()/2..]) { return; }
 
-    println!("String repetition is removed(string is halved).");
+        drop(chars);
+        println!(">>>Action:");
+        if set_clipboard(&text[..len/2]).is_err() {
+            println!("Failed to update clipboard");
+        }
+
+        println!("String repetition is removed(string is halved).");
+    }
 }
 
 fn main() {
     println!("####################################");
     println!("#  Senkou Text repetition remover  #");
     println!("####################################");
-    ClipboardManager::new().delay(10).ok_callback(handler_clip_text).run();
+    ClipboardManager::new().delay(1).ok_callback(handler_clip_text).run();
 }
