7c0535c53c1f484c9187a0296729d2e8d8cbae6a
Douman <douman@gmx.se>
2015-12-05 17:31:14 +0300

    Evolimit text corrector

---
+++ rust/clipboard_monitor/src/evolimit.rs
@@ -0,0 +1,34 @@
+//! Evolimit VN text corrector.
+
+extern crate clipboard_win;
+use clipboard_win::{set_clipboard, ClipboardManager};
+pub mod utils;
+
+fn handler_clip_text(text: &String) {
+    let mut update = false;
+    let mut text = text.clone();
+    if !utils::is_jp(&text) { return; }
+
+    const DOT: &'static str = "・";
+    if let Some(_) = text.find(DOT) {
+        update = true;
+        text = text.replace(DOT, "");
+    }
+
+    if update {
+        println!(">>>Action:");
+        if set_clipboard(&text).is_err() {
+            println!("Hmph... failed to update clipboard");
+        }
+        else {
+            println!("Text is cleaned");
+        }
+    }
+}
+
+fn main() {
+    println!("####################################");
+    println!("#     Evolimit text corrector      #");
+    println!("####################################");
+    ClipboardManager::new().delay(10).ok_callback(handler_clip_text).run();
+}
