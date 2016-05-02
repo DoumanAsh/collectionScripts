9d0f5f8542b3701feb77368ee7f5f1f875f6432a
Douman <douman@gmx.se>
2016-05-02 09:58:48 +0300

    Rust: eushully nocd refactoring

--- rust/eushully/src/nocd.rs
+++ rust/eushully/src/nocd.rs
@@ -2,39 +2,11 @@ extern crate windows_win;
 
 use windows_win::*;
 
-const GAME_ENGINE: &'static str = "ARCGameEngine";
-
-fn hex_str_to_bytes(hex: &str) -> Vec<u8> {
-    let mut result = Vec::new();
-    let mut modulus = 0;
-    let mut buf = 0;
-
-    for (idx, byte) in hex.replace(" ", "").bytes().enumerate() {
-        buf <<= 4;
-
-        match byte {
-            b'A'...b'F' => buf |= byte - b'A' + 10,
-            b'a'...b'f' => buf |= byte - b'a' + 10,
-            b'0'...b'9' => buf |= byte - b'0',
-            b' '|b'\r'|b'\n'|b'\t' => {
-                buf >>= 4;
-                continue
-            },
-            _ => {
-                let ch = hex[idx..].chars().next().unwrap();
-                panic!(format!("Invalid character={}", ch));
-            }
-        }
-
-        modulus += 1;
-        if modulus == 2 {
-            modulus = 0;
-            result.push(buf);
-        }
-    }
-
-    result
-}
+mod common;
+use common::{
+    GAME_ENGINE,
+    hex_str_to_bytes
+};
 
 use std::io::BufRead;
 ///Read patches from config file or sets default.
