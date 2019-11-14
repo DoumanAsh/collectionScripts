595a43ce9524a9fb8db92dd45b79a33de5f70a58
Douman <douman@gmx.se>
2019-11-14 17:27:30 +0100

    PS: Rust utils

---
+++ powershell/rust_utils.ps1
@@ -0,0 +1,14 @@
+
+<#
+    Retrieves default toolchain from rustup
+#>
+function rust_get_default_channel() {
+    if (Get-Command "rustup" -ErrorAction SilentlyContinue) {
+        $default = rustup default | Select-String -Pattern "([^-]+)-.+ \(default\)"
+        $default = $default.matches.groups[1]
+
+        echo "$default"
+    } else {
+        echo ""
+    }
+}
