9572e2609f4ce1f90598139cafa88b84c1918933
Douman <douman@gmx.se>
2019-12-15 12:21:50 +0100

    PS1: Improve prompt

--- powershell/prompt.ps1
+++ powershell/prompt.ps1
@@ -1,18 +1,34 @@
 # Simple ps prompt with git status
 function prompt {
-    $git_out = $(git branch 2> $null)
-    if ([string]::IsNullOrEmpty($git_out)) {
+    $git_branch = $(git rev-parse --abbrev-ref HEAD 2> $null)
+    if ([string]::IsNullOrEmpty($git_branch)) {
         "$ESC[33m$($executionContext.SessionState.Path.CurrentLocation)$ESC[0m$("`n$(Get-Date -Format 'HH:mm') $" * ($nestedPromptLevel + 1)) "
     } else {
-        $git_out = "$git_out" | Select-String -Pattern "\* (.+)" | % { $_.matches.groups[1].value }
-        git diff --no-ext-diff --quiet --exit-code
+        $git_dirt = $(git diff-index --name-only --ignore-submodules HEAD --)
 
-        if ($?) {
+        $git_commits_to_push = $(git rev-list head...origin/$git_branch  --left-right --ignore-submodules --count)
+        $git_commits_ahead,$git_commits_behind = $git_commits_to_push.Split('')
+
+        if ([string]::IsNullOrEmpty($git_dirt)) {
             $status_color = "$ESC[32m"
         } else {
             $status_color = "$ESC[31m"
         }
 
-        "$ESC[33m$($executionContext.SessionState.Path.CurrentLocation)$ESC[0m [$status_color$git_out$ESC[0m]$("`n$(Get-Date -Format 'HH:mm') $" * ($nestedPromptLevel + 1)) "
+        $commit_diff_line = ""
+
+        if ([int]$git_commits_ahead -gt 0) {
+            $commit_diff_line = " ^ $git_commits_ahead"
+        }
+
+        if ([int]$git_commits_behind -gt 0) {
+            if ([string]::IsNullOrEmpty($commit_diff_line)) {
+                $commit_diff_line = " ^ -$git_commits_behind"
+            } else {
+                $commit_diff_line = "$commit_diff_line -$git_commits_behind"
+            }
+        }
+
+        "$ESC[33m$($executionContext.SessionState.Path.CurrentLocation)$ESC[0m [$status_color$git_branch$commit_diff_line$ESC[0m]$("`n$(Get-Date -Format 'HH:mm') $" * ($nestedPromptLevel + 1)) "
     }
 }
