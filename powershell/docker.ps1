52f3376fa73ec49c0b36d88ec322f72139aa1888
DoumanAsh <douman@gmx.se>
2022-08-26 13:02:46 +0900

    docker: Add docker script extra options to customize usage of docker sock

--- powershell/docker.ps1
+++ powershell/docker.ps1
@@ -3,17 +3,24 @@ function run_docker {
         [string]$tag,
         [switch]$h,
         [switch]$help,
-        [string]$mount = ${pwd}
+        [switch]$docker_sock,
+        [string]$mount = ${pwd},
+        [string]$name = $tag
     )
 
     if ($h -Or $help -Or [string]::IsNullOrEmpty($tag)) {
-        echo "Usage: docker [-mount <folder>] <tag>"
+        echo "Usage: docker [-mount <folder>] [-docker_sock] <tag>"
         return
     }
 
-    $name = $tag -replace '/','_' -replace ':','_'
+    $name = $name -replace '/','_' -replace ':','_'
 
-    $cmd = "docker run --rm --network host --name $name -v ${mount}:/mount -it $tag"
+    $extra_args = ""
+    if ($docker_sock) {
+        $extra_args = "$extra_args -v //var/run/docker.sock:/var/run/docker.sock"
+    }
+
+    $cmd = "docker run --rm --network host --name $name -v ${mount}:/mount $extra_args -it $tag"
     echo "> $cmd"
     iex $cmd
 }
