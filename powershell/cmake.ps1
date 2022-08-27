d322a92b7e7e4ed6f4927d9b514e4906d5582248
DoumanAsh <douman@gmx.se>
2022-08-27 10:33:46 +0900

    Ps1: Improve cmake command to pick up vcpkg toolchain file

--- powershell/cmake.ps1
+++ powershell/cmake.ps1
@@ -1,22 +1,29 @@
-if (Get-Command "ninja" -ErrorAction SilentlyContinue) {
-    $generator = "Ninja"
-}
-else {
-    $generator = "NMake Makefiles"
-}
-
-if (Get-Command "vcpkg" -ErrorAction SilentlyContinue) {
-    $vcpkg_dir = split-path -parent (Get-Command "vcpkg" | Select-Object -ExpandProperty Definition)
-
-    function cmake_vcpkg() {
-        cmake -G $generator -DCMAKE_TOOLCHAIN_FILE="$vcpkg_dir\scripts\buildsystems\vcpkg.cmake" $args
-    }
-}
-
 function cmake_gen() {
-    cmake -G $generator $args
+    if (Get-Command "ninja" -ErrorAction SilentlyContinue) {
+        $generator = "Ninja"
+    } else {
+        $generator = "NMake Makefiles"
+    }
+
+
+    if (Get-Command "vcpkg" -ErrorAction SilentlyContinue) {
+        $vcpkg_dir = split-path -parent (Get-Command "vcpkg" | Select-Object -ExpandProperty Definition)
+        $vcpkg_toolchain_path = Join-Path -Path $vcpkg_dir -ChildPath scripts | Join-Path -ChildPath buildsystems | Join-Path -ChildPath vcpkg.cmake
+
+        if (Test-Path -Path $vcpkg_toolchain_path) {
+            $vcpkg_toolchain = '-DCMAKE_TOOLCHAIN_FILE="{0}"' -f $vcpkg_toolchain_path
+        } else {
+            $vcpkg_toolchain = ''
+        }
+    } else {
+        $vcpkg_toolchain = ''
+    }
+
+    echo ">cmake -G $generator $vcpkg_toolchain $args"
+    cmake -G $generator $vcpkg_toolchain $args
 }
 
 function cmake_build() {
+    echo ">cmake --build $args"
     cmake --build $args
 }
