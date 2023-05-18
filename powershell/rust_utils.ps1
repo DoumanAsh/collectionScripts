79791fa208c0237b57835518edad5236839cedc4
DoumanAsh <douman@gmx.se>
2023-05-18 12:10:15 +0900

    powershell: Add script to clean up rust target folders

--- powershell/rust_utils.ps1
+++ powershell/rust_utils.ps1
@@ -12,3 +12,46 @@ function rust_get_default_channel() {
         echo ""
     }
 }
+
+<#
+    .Description
+    Performs cleanup of all `target` folders for every crate
+#>
+function rust_clean_repos() {
+    Param (
+        [string]$folder,
+        [switch]$h,
+        [switch]$y,
+        [switch]$help
+    )
+
+    if ($h -Or $help) {
+        echo "Usage: rust_clean_repos [folder] [-y]"
+        return
+    }
+    if ([string]::IsNullOrEmpty($folder)) {
+        $folder = Get-Location
+    }
+
+    Get-ChildItem $folder -Recurse -Filter Cargo.toml -ErrorAction SilentlyContinue | Foreach-Object {
+        $crate = Split-Path -Path "$($_.FullName)"
+        $crate_target = Join-Path -Path "$crate" -ChildPath "target"
+
+        if (Test-Path -Path $crate_target) {
+            $title    = $crate_target
+            $question = 'Are you sure you want to delete it?'
+            $choices  = '&Yes', '&No'
+
+            if ($y) {
+                Remove-Item -Recurse -Force $crate_target
+                echo "$($crate_target): Deleted..."
+            } else {
+                $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+                    if ($decision -eq 0) {
+                        Remove-Item -Recurse -Force $crate_target
+                        echo "Deleted"
+                    }
+            }
+        }
+    }
+}
